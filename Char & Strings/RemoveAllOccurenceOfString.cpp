cf5b40af19d6260830481265d2b851eba4449d8b
Krati <krati21csu361@ncuindia.edu>
2024-04-08 21:17:33 +0530

    commit

---
+++ Char & Strings/RemoveAllOccurenceOfString.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string removeOccurence( string s , string part ){
+    while ( s.find( part ) != string::npos){
+        s.erase(s.find(part) , part.length());
+    }
+    return s ; 
+}
+
+int main(){
+    string s = "aabcbabccbaba"; 
+    string part = "cba"; 
+    string remove = removeOccurence(s ,part);
+    cout << remove << endl;
+}
